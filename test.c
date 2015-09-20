2dbee755c04b959305b26776da9d9cf94073eba9
drzeno <drzeno@yeah.net>
2015-09-20 11:33:16 +0800

    -1 is true

---
+++ test.c
@@ -0,0 +1,14 @@
+/*********************************
+    > File Name: test.c
+    > Author: Zeno
+    > Mail:340562424@qq.com 
+    > Created Time: 2015年09月20日 星期日 11时28分23秒
+ ********************************/
+#include<stdio.h>
+int main(void)
+{
+	if(-1)
+		printf("-1 is true");
+	else
+		printf("-1 is false");
+}
